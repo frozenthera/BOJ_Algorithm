ef8bd0c21536edda16869849c6cfb7182b977c09
frozenthera <50418378+frozenthera@users.noreply.github.com>
2023-08-12 02:50:27 +0900

    [Silver III] Title: N과 M (2), Time: 52 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -5,17 +5,14 @@ serial = []
 
 def func(series, left, curPos):
     if left <= 0:
-        for i in range(len(series)):
-            print(series[i], end= ' ')
-        print()
+        print(*series)
         return
     if curPos > N and left > 0:
         return
     
-    
     series.append(curPos)
     func(series, left-1, curPos+1)
-    series.pop(len(series)-1)
+    series.pop()
     func(series, left, curPos+1)
 
 func(serial, M, 1)
