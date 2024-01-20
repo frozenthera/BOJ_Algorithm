a14b2ae74a677ffbfce6ece13cfaa2d9eb5178b4
frozenthera <50418378+frozenthera@users.noreply.github.com>
2024-01-21 02:13:44 +0900

    [Silver III] Title: N과 M (8), Time: 8 ms, Memory: 2080 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int N, M;
+vector<int> example;
+void print_vec(vector<int> vec)
+{
+	for_each(vec.begin(), vec.end(), [](const auto& e) {cout << e << " "; });
+	cout << "\n";
+}
+
+void func(vector<int> vec, int curIdx)
+{
+	if (curIdx > N-1)
+	{
+		return;
+	}
+	if (vec.size() == M)
+	{
+		print_vec(vec);
+	}
+	else
+	{
+		vec.push_back(example[curIdx]);
+		func(vec, curIdx);
+		vec.pop_back();
+		func(vec, curIdx + 1);
+	}
+}
+
+int main()
+{
+	cin >> N >> M;
+	cin.ignore();
+
+	string str;
+	getline(cin, str);
+	istringstream ss(str);
+	string stringBuffer;
+
+	while (getline(ss, stringBuffer, ' ')) {
+		example.push_back(stoi(stringBuffer));
+	}
+
+	sort(example.begin(), example.end());
+
+	vector<int> queue;
+	func(queue, 0);
+}
