b5a6d1b1ba266bd6fc430aa79e32d0b58b941955
frozenthera <50418378+frozenthera@users.noreply.github.com>
2024-01-29 16:48:28 +0900

    [Silver I] Title: 경로 찾기, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	int N;
+	cin >> N;
+	vector<vector<int>> graph(N, vector<int>(N, 0));
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			int temp;
+			cin >> temp;
+			if (temp == 1) graph[i][j] = 1;
+		}
+	}
+
+	for (int k = 0; k < N; k++)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				if (graph[i][k] == 1 && graph[k][j] == 1)
+				{
+					graph[i][j] = 1;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << graph[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
