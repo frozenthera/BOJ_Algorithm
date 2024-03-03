405b9e6261738042c7916ab7a79b8d48d20e7d1a
frozenthera <50418378+frozenthera@users.noreply.github.com>
2024-03-03 23:22:33 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main()
+{
+    int N;
+    cin >> N;
+    for(int i =1; i<= N; i++)
+    {
+        cout << "Hello World, Judge " << i << "!\n";
+    }
+}
